b049b9b79b02808547c0508d1cd624d85f5a8048
jeremyperonto <jeremy@peronto.com>
2015-02-18 21:44:41 -0800

    Initial Commit

---
+++ Waxpacked/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Waxpacked
+//
+//  Created by Jeremy Peronto on 2/18/15.
+//  Copyright (c) 2015 Jeremy Peronto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
