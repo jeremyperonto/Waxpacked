69b32f787607a07f8b7eaeee0e9c050e7b87be6e
jeremyperonto <jeremy@peronto.com>
2015-02-22 21:57:27 -0800

    Revert 2bcc3f2..f370e00
    This rolls back to commit 2bcc3f2c4977f16479d319424ac0107f1ad28c39.

--- Waxpacked-Bridging-Header.h
+++ Waxpacked-Bridging-Header.h
@@ -2,5 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <Parse/Parse.framework>
+#import <Parse/Parse.h>
 #import <ParseUI/ParseUI.h>
